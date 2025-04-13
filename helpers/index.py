24cebcb8c0440dfd1531ecab087c3ac7d5f37505
Manish Suthar <manishsuthar078@gmail.com>
2025-04-13 18:07:48 +0530

    refactor: comments added to all the helpers

---
+++ helpers/index.py
@@ -0,0 +1,59 @@
+# search_files(): [Audio] or [Playlist]
+# list_dir_and_files(): [Audio], [Playlist], and [DIR]
+# generate_playlist(): Creates .m3u
+# edit_file(): Edits .m3u files
+# get_metadata(): Returns metadata of file.
+
+
+# Search for audio and playlist files in a directory
+# Search must be within allowed directories and valid path
+def search_files():
+    try:
+        pass
+    except Exception:
+        pass
+
+
+# @security: Checking if the path is valid or not
+# By checking, If the path is a valid string
+# By checking, If the path is actually a path in PC or not
+# By checking, If the path comes under the allowed directory
+def validatePath():
+    try:
+        pass
+    except Exception:
+        pass
+
+
+# Make line-based edits to a `.m3u` file.
+# Add, remove, or re-order songs in the playlist.
+def edit_file():
+    try:
+        pass
+    except Exception:
+        pass
+
+
+# Generate a `.m3u` file, Add all the `media` that has been categorised.
+def generate_playlist():
+    try:
+        pass
+    except Exception:
+        pass
+
+
+# @Returns metadata of the media file, including: Author, Album, Title, Genre, Year, Duration, and Bitrate.
+# Other infomation like: size, name, last modified time, permissions, and type.
+def get_metadata():
+    try:
+        pass
+    except Exception:
+        pass
+
+
+# @Returns a detailed listing of all files and directories in a specified path.
+def list_dir_and_files():
+    try:
+        pass
+    except Exception:
+        pass
